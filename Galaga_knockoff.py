0dcbf0f992d0b25d45b8377e7190a59b6adb8e56
ateddy624-collab <ateddy624@phm.k12.in.us>
2026-02-23 09:49:23 -0500

    Create Galaga_knockoff.py

---
+++ Galaga_knockoff.py
@@ -0,0 +1,14 @@
+#Imports
+import tkinter as tk
+import time
+from tkinter import font
+import random
+
+WIDTH = 600
+HEIGHT = 400
+standard_enemy_color = 
+shooting_enemy_color = 
+swooping_enemy_color = 
+enemy_highlights = 
+player_color = 
+player_highlights = 
